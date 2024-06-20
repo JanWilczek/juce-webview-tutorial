a8b727408271b911fa44e3823d1a6d52fb13c8ec
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-06-20 09:27:39 +0200

    Navigate to your own ResourceProvider

--- plugin/source/PluginEditor.cpp
+++ plugin/source/PluginEditor.cpp
@@ -1,14 +1,25 @@
 #include "JuceWebViewTutorial/PluginEditor.h"
+#include <optional>
 #include "JuceWebViewTutorial/PluginProcessor.h"
+#include "juce_core/juce_core.h"
+#include "juce_gui_extra/juce_gui_extra.h"
 
 namespace audio_plugin {
 AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(
-    AudioPluginAudioProcessor &p)
-    : AudioProcessorEditor(&p), processorRef(p) {
+    AudioPluginAudioProcessor& p)
+    : AudioProcessorEditor(&p),
+      processorRef(p),
+      webView(juce::WebBrowserComponent::Options{}.withResourceProvider(
+          [](const auto& url) {
+            juce::ignoreUnused(url);
+            DBG("ResourceProvider called with " + url);
+            return std::nullopt;
+          })) {
   juce::ignoreUnused(processorRef);
 
   addAndMakeVisible(webView);
-  webView.goToURL("https://juce.com");
+  // webView.goToURL("https://juce.com");
+  webView.goToURL(webView.getResourceProviderRoot());
 
   setResizable(true, true);
   setSize(800, 600);
@@ -19,4 +30,4 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor() {}
 void AudioPluginAudioProcessorEditor::resized() {
   webView.setBounds(getBounds().withTrimmedLeft(getWidth() / 2));
 }
-} // namespace audio_plugin
+}  // namespace audio_plugin
