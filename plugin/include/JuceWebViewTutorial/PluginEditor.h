bb03b5807fc7c9381e574fcc4843bfa092261936
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-06-20 08:39:30 +0200

    Added a WebBrowserComponent

--- plugin/include/JuceWebViewTutorial/PluginEditor.h
+++ plugin/include/JuceWebViewTutorial/PluginEditor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "PluginProcessor.h"
+#include <juce_gui_extra/juce_gui_extra.h>
 
 namespace audio_plugin {
 
@@ -13,9 +14,9 @@ public:
   void resized() override;
 
 private:
-  // This reference is provided as a quick way for your editor to
-  // access the processor object that created it.
   AudioPluginAudioProcessor &processorRef;
+
+  juce::WebBrowserComponent webView;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
