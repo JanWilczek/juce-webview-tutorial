cb8d227726f3503111e5872663731d79a35b56ea
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-06-20 10:53:42 +0200

    Read index.html in the editor

--- plugin/source/PluginEditor.cpp
+++ plugin/source/PluginEditor.cpp
@@ -2,9 +2,22 @@
 #include <optional>
 #include "JuceWebViewTutorial/PluginProcessor.h"
 #include "juce_core/juce_core.h"
+#include "juce_graphics/juce_graphics.h"
 #include "juce_gui_extra/juce_gui_extra.h"
 
 namespace audio_plugin {
+namespace {
+std::vector<std::byte> streamToVector(juce::InputStream& stream) {
+  const auto sizeInBytes = static_cast<size_t>(stream.getTotalLength());
+  std::vector<std::byte> result(sizeInBytes);
+  stream.setPosition(0);
+  [[maybe_unused]] const auto bytesRead =
+      stream.read(result.data(), result.size());
+  jassert(bytesRead == static_cast<ssize_t>(sizeInBytes));
+  return result;
+}
+}  // namespace
+
 AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(
     AudioPluginAudioProcessor& p)
     : AudioProcessorEditor(&p),
@@ -34,6 +47,23 @@ void AudioPluginAudioProcessorEditor::resized() {
 auto AudioPluginAudioProcessorEditor::getResource(const juce::String& url) const
     -> std::optional<Resource> {
   DBG("ResourceProvider called with " + url);
+
+  static const auto resourceFilesRoot =
+      juce::File::getSpecialLocation(
+          juce::File::SpecialLocationType::currentApplicationFile)
+          .getParentDirectory()
+          .getParentDirectory()
+          .getChildFile("public");
+
+  DBG("Resource files root is " + resourceFilesRoot.getFullPathName());
+
+  if (url == "/") {
+    const auto indexHtml =
+        resourceFilesRoot.getChildFile("index.html").createInputStream();
+    jassert(indexHtml != nullptr);
+    return Resource{streamToVector(*indexHtml), juce::String{"text/html"}};
+  }
+
   return std::nullopt;
 }
 }  // namespace audio_plugin
