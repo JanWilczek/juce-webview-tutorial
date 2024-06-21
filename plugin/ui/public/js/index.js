c5fcacf9b3751617afc18aeff1c7c5f25925776e
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-06-21 18:39:31 +0200

    Removed unused variable

--- plugin/ui/public/js/index.js
+++ plugin/ui/public/js/index.js
@@ -2,7 +2,7 @@ import * as Juce from "./juce/index.js";
 
 console.log("JUCE frontend library successfully imported");
 
-const _ = window.__JUCE__.backend.addEventListener("exampleevent", (objectFromBackend) => { console.log(objectFromBackend); });
+window.__JUCE__.backend.addEventListener("exampleevent", (objectFromBackend) => { console.log(objectFromBackend); });
 
 const data = window.__JUCE__.initialisationData;
 
