c40aa8984c66d525c4539aced71d29852a956b91
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-06-24 19:32:49 +0200

    Formatting

--- plugin/ui/public/js/index.js
+++ plugin/ui/public/js/index.js
@@ -2,7 +2,12 @@ import * as Juce from "./juce/index.js";
 
 console.log("JUCE frontend library successfully imported");
 
-window.__JUCE__.backend.addEventListener("exampleevent", (objectFromBackend) => { console.log(objectFromBackend); });
+window.__JUCE__.backend.addEventListener(
+  "exampleevent",
+  (objectFromBackend) => {
+    console.log(objectFromBackend);
+  }
+);
 
 const data = window.__JUCE__.initialisationData;
 
@@ -15,7 +20,7 @@ const nativeFunction = Juce.getNativeFunction("nativeFunction");
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("nativeFunctionButton");
   button.addEventListener("click", () => {
-    nativeFunction("one", 2, null).then(result => {
+    nativeFunction("one", 2, null).then((result) => {
       console.log(result);
     });
   });
@@ -24,14 +29,16 @@ document.addEventListener("DOMContentLoaded", () => {
   let emittedCount = 0;
   emitEventButton.addEventListener("click", () => {
     emittedCount++;
-    window.__JUCE__.backend.emitEvent("exampleJavaScriptEvent", { emittedCount : emittedCount });
+    window.__JUCE__.backend.emitEvent("exampleJavaScriptEvent", {
+      emittedCount: emittedCount,
+    });
   });
 
   const slider = document.getElementById("gainSlider");
   const sliderState = Juce.getSliderState("GAIN");
-  slider.oninput = function() {
+  slider.oninput = function () {
     sliderState.setNormalisedValue(this.value);
-  }
+  };
 
   slider.step = 1 / sliderState.properties.numSteps;
 
@@ -41,25 +48,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const bypassCheckbox = document.getElementById("bypassCheckbox");
   const bypassToggleState = Juce.getToggleState("BYPASS");
-  bypassCheckbox.oninput = function() {
+  bypassCheckbox.oninput = function () {
     bypassToggleState.setValue(this.checked);
-  }
+  };
   bypassToggleState.valueChangedEvent.addListener(() => {
     bypassCheckbox.checked = bypassToggleState.getValue();
   });
 
-  const distortionTypeComboBox = document.getElementById("distortionTypeComboBox");
-  const distortionTypeComboBoxState = Juce.getComboBoxState("distortionTypeComboBox");
+  const distortionTypeComboBox = document.getElementById(
+    "distortionTypeComboBox"
+  );
+  const distortionTypeComboBoxState = Juce.getComboBoxState(
+    "distortionTypeComboBox"
+  );
   distortionTypeComboBoxState.propertiesChangedEvent.addListener(() => {
-    distortionTypeComboBoxState.properties.choices.forEach(choice => {
+    distortionTypeComboBoxState.properties.choices.forEach((choice) => {
       distortionTypeComboBox.innerHTML += `<option value=\"${choice}\">${choice}</option>`;
     });
   });
   distortionTypeComboBoxState.valueChangedEvent.addListener(() => {
-    distortionTypeComboBox.selectedIndex = distortionTypeComboBoxState.getChoiceIndex();
+    distortionTypeComboBox.selectedIndex =
+      distortionTypeComboBoxState.getChoiceIndex();
   });
-  distortionTypeComboBox.oninput = function() {
+  distortionTypeComboBox.oninput = function () {
     distortionTypeComboBoxState.setChoiceIndex(this.selectedIndex);
-  }
+  };
 });
-
