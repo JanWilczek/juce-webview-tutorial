5fc24bd5e2cda06fae63165f07bb97f2ac3a5a41
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-06-25 12:29:55 +0200

    Use API V3 of nuget.org.

--- scripts/DownloadWebView2.ps1
+++ scripts/DownloadWebView2.ps1
@@ -3,6 +3,6 @@ $packageSourceName = "nugetRepository"
 # Ignore errors: they are meaningless
 $ErrorActionPreference = "SilentlyContinue"
 
-Register-PackageSource -Provider NuGet -Name $packageSourceName -Location https://www.nuget.org/api/v2 -Confirm
+Register-PackageSource -Provider NuGet -Name $packageSourceName -Location https://api.nuget.org/v3/index.json -Confirm
 Install-Package Microsoft.Web.WebView2 -Scope CurrentUser -RequiredVersion 1.0.1901.177 -Source $packageSourceName -Confirm
 
