d2f2f7196d056944880559636e0db6d1f45f2fda
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-06-25 08:26:45 +0200

    Added plotly.js as a dependency.

--- plugin/ui/public/js/index.js
+++ plugin/ui/public/js/index.js
@@ -73,4 +73,10 @@ document.addEventListener("DOMContentLoaded", () => {
   distortionTypeComboBox.oninput = function () {
     distortionTypeComboBoxState.setChoiceIndex(this.selectedIndex);
   };
+
+  // Plot with Plotly
+  Plotly.newPlot("gd", /* JSON object */ {
+    "data": [{ "y": [1, 2, 3] }],
+    "layout": { "width": 600, "height": 400}
+  });
 });
